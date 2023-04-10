577d4445ea74412c08e739a194e3c99fffe94edd
MrLucasAlmeida <lucasoa2701@live.com>
2023-04-09 20:41:42 -0700

    fixed login and signup

--- server.js
+++ server.js
@@ -18,11 +18,16 @@ app.use(express.static('public_html'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// connect to mongoDB
 const mongoURL = 'mongodb://127.0.0.1:27017/quizzy';
 mongoose.connect(mongoURL, { useNewUrlParser: true });
 mongoose.connection.on("connected", () => console.log("Connected to MongoDB"))
 mongoose.connection.on("error", (err) => console.log(err))
 
+
+
+// schemas
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema({
@@ -36,50 +41,58 @@ const usersSchema = new Schema({
 const Users = mongoose.model("Users", usersSchema);
 
 
-app.post('/login', (req, res, next) => {
-    console.log("TRying to login")
-    const username = req.body.username;
-    const password = req.body.password;
-    Users.findOne({username: username}).exec()
-        .then(user =>{
-            if (user){
-                var hash = crypto.createHash('sha3-256');
-                var toHash = password + user.salt;
-                dataa = hash.update(toHash, 'utf-8');
-                hashed = dataa.digest('hex');
-                console.log(user.hash)
-                console.log(hashed)
-                if (hashed === user.hash){
-                    console.log("success")
-                    res.redirect("/home.html")
-                }
-                else{
-                    console.log("incorrect password")
-                }
-            }
-        })
 
-})
+// routes
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/signup', (req, res, next) => {
+// route for login in to the website
+app.post('/login', async (req, res) => {
+    console.log("Trying to login");
+    const { username, password } = req.body;
+    
+    // find user with that username
+    const response = await Users.findOne({username: username}).exec();
+    
+    // check if there is a user with that username
+    if (!response) {
+        res.redirect("/");
+        return;
+    }
+
+    // hash the password
+    var hash = crypto.createHash('sha3-256');
+    var toHash = password + response.salt;
+    dataa = hash.update(toHash, 'utf-8');
+    hashed = dataa.digest('hex');
+
+    // redirect to home page if password is correct
+    if (hashed === response.hash){
+        res.redirect("/home.html");
+    } else {
+        res.redirect("/");
+    }
+});
+
+// route for signing up to the website
+app.post('/signup', async (req, res) => {
     var hash = crypto.createHash('sha3-256');
     const {username, password} = req.body;
-    Users.findOne({username: username}).exec()
-        .then(user => {
-            if (user){
-                console.log("This user already exists")
-            }
-            else{
-                var salt = Math.floor(Math.random() * 100000);
-                var toHash = password + salt;
-                data = hash.update(toHash, 'utf-8');
-                gen_hash = data.digest('hex');
-                const user = new Users({username, salt, hash: gen_hash})
-                user.save();
-                res.json(user)
-                res.redirect("/index.html")
-            }
-        })
+    const response = await Users.findOne({username}).exec();
+
+    // stop if user already exists
+    if (response) {
+        res.redirect("/");
+        return;
+    }
+
+    // add new user
+    let salt = Math.floor(Math.random() * 1000000);
+    let toHash = password + salt;
+    let data = hash.update(toHash, 'utf-8');
+    let gen_hash = data.digest('hex');
+    const user = new Users({username, salt, hash: gen_hash, favorites: [], listings: []})
+    user.save();
+    res.redirect("/");
 })
 
 app.listen(port, () => {
