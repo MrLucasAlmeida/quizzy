888b036c2eb5f71c110e88e444cf3c85fe1fc079
sodikovakbarali <112781170+sodikovakbarali@users.noreply.github.com>
2023-04-10 11:19:28 -0700

    server.js

--- public_html/scripts/signUp.js
+++ public_html/scripts/signUp.js
@@ -12,33 +12,16 @@ async function handleCreateAccount() {
     });
     // const data = JSON.stringify(response);
     // console.log(data);
+    if (response.status === 200){
+        window.location.href = '/login';
+    }
+    else{
+        const p = document.createElement('p')
+        p.textContent = "User already exists! Try another username"
+        p.style.fontSize = '15px';
+        p.style.color = 'red';
+        signupForm.appendChild(p)
+        const myTimeout = setTimeout(() => {signupForm.removeChild(p)}, 2000)
+    }
+}
 
-async function handleCreateAccount() {
-    console.log('trying to create account')
-    const username = document.getElementById('signupUsername').value;
-    const password = document.getElementById('signupPassword').value;
-    const response = await fetch('/signUp', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-            },
-        body: JSON.stringify({username, password})
-    });
-    // const data = JSON.stringify(response);
-    // console.log(data);
-
-const form = document.querySelector(form)
-form.addEventListener('submit', (event)=>{
-    event.preventDefault();
-    fetch('/signUp')
-        .then(response => {
-            if (response.status === 404){
-                const p = document.createElement('p')
-                p.textContent = "User already exists! Try another username"
-                p.style.fontSize = '15px';
-                p.style.color = 'red';
-                signupForm.appendChild(p)
-                const myTimeout = setTimeout(() => {signupForm.removeChild(p)}, 2000)
-            }
-        })
-})
