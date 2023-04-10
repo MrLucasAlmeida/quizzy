9aa9b9893a1c913086c64cc7a53160113034b0a5
sodikovakbarali <112781170+sodikovakbarali@users.noreply.github.com>
2023-04-10 11:46:10 -0700

    c

---
+++ public_html/scripts/index.js
@@ -0,0 +1,31 @@
+async function handleLoginAccount() {
+    console.log('trying to create account')
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    // do nothing if either field is empty
+    if (username === '' || password === '') return;
+
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify({username, password})
+    });
+    // const data = JSON.stringify(response);
+    // console.log(data);
+    if (response.status === 200){
+        window.location.href = './home.html';
+    }
+    else{
+        window.location.href = '/';
+        const loginForm = document.getElementById('loginForm');
+        const p = document.createElement('p')
+        p.textContent = "You failed "
+        p.style.fontSize = '15px';
+        p.style.color = 'red';
+        loginForm.appendChild(p)
+        const myTimeout = setTimeout(() => {signupForm.removeChild(p)}, 2000)
+    }
+}
