62af7ea6023d164ad9a42817813e42c873250909
MrLucasAlmeida <lucasoa2701@live.com>
2023-04-18 13:48:01 -0700

    simple script for checking for theme

---
+++ public_html/scripts/themeToggle.js
@@ -0,0 +1,13 @@
+//  check local storage for theme
+
+const theme = localStorage.getItem('theme');
+console.log(theme);
+if (theme === 'dark') {
+    // change the theme to dark
+    const allElements = document.querySelectorAll('*');
+    for (let i in allElements) {
+        allElements[i].classList.add('dark-theme');
+    }
+}
+
+
