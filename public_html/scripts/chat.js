d4423a61b9f0bbcbe59beb763c3bf396ff0be44a
sodikovakbarali <112781170+sodikovakbarali@users.noreply.github.com>
2023-04-23 16:51:34 -0700

    chat feature

--- public_html/scripts/chat.js
+++ public_html/scripts/chat.js
@@ -3,23 +3,29 @@ const MASTER_URL = 'http://localhost:3000';
 const findConversation = document.getElementById('findConversation');
 
 var otherUser;
-var iteration = false;
+
 // when the button is clicked, the messages between two users have to displayed while fetching their conversation
 findConversation.addEventListener('click', () => {
     // fetch the conversation between the current user and the user in the input
     const user = document.getElementById('userName').value;
-    
+    console.log(user)
+    console.log("Being clicked")
     otherUser = user;
-    
-    displayConversation(otherUser, iteration);
+    panel.innerHTML = '';
+    writeMessageD();
+    setInterval(() => {
+        displayConversation(otherUser);
+    }, 1000);
+
+    displayConversation(otherUser);
 
 })
 
 
 
-async function displayConversation(user, iteration)
+async function displayConversation(user)
 {
-    const panel = document.getElementById('panel');
+    const conversationPanel = document.getElementById('conversationContainer')
     console.log('displaying conversation')
     
     fetch(`${MASTER_URL}/get/conversation/${user}`)
@@ -33,58 +39,39 @@ async function displayConversation(user, iteration)
         .then(conversation => {
             // display the conversation
             const sortedConversation = conversation.messages.sort((a,b) => a.time - b.time)
-            if (iteration){
-                let conversation = sortedConversation[sortedConversation.length - 1];
-                const newMessage = document.getElementsByClassName('newMessage')[document.getElementsByClassName('newMessage').length - 1];
-
+            conversationPanel.innerHTML = '';
+            for (let i in sortedConversation){
+                let conversation = sortedConversation[i];
                 const div = document.createElement('div');
-                const input = document.getElementById('newMessage');
-                const submit = document.getElementById('sendMessage');
-                div.className ='message';
+                div.className = 'message';
                 div.innerHTML = `
                     <h5 class="username">${conversation.sender}</h5>
                     <h5 class="content">${conversation.message}</h5>
                 `;
-                console.log(newMessage)
-                // remove the input and submit button and add the new message
-    
-                newMessage.removeChild(submit)
-                newMessage.removeChild(input);
-                // I tried using insert before, but didn't work
+                conversationPanel.appendChild(div);
             }
-            else{
-                panel.innerHTML = '';
-                for (let i in sortedConversation){
-                    
-                    let conversation = sortedConversation[i];
-                    const div = document.createElement('div');
-                    div.className = 'message';
-                    div.innerHTML = `
-                        <h5 class="username">${conversation.sender}</h5>
-                        <h5 class="content">${conversation.message}</h5>
-                    `;
-                    panel.append(div);
-                }
-            }
-            writeMessageD();
-            
-        })
-        .catch(error => {
-            console.log(error);
-        })
+            })
     
 } 
 
 async function writeMessageD(){
-    let newDiv = document.createElement('div');
+    console.log('writing message')
     const panel = document.getElementById('panel');
+    console.log(panel)
+    const chatDiv = document.createElement('div');
+    chatDiv.id = 'conversationContainer';
+    let newDiv = document.createElement('div');
     newDiv.className = 'newMessage';
     newDiv.innerHTML = `<input type="text" id="newMessage" placeholder="Write a message">
     <button id="sendMessage" onclick="sendMessage()">Send</button>`
-    panel.append(newDiv)
+    panel.appendChild(chatDiv)
+    panel.appendChild(newDiv);
+    console.log(panel)
+    
 }
 
 async function sendMessage(){
+
     const message = document.getElementById('newMessage').value;
     console.log(message);
     // fetch to post the message
@@ -98,5 +85,5 @@ async function sendMessage(){
     // clear the input
     iteration = true;
     document.getElementById('newMessage').value = '';
-    displayConversation(otherUser, iteration);
+    displayConversation(otherUser);
 }
